db21d70d381ca1ba5026894308c35d79fdc37cd3
jackyscript <8930712+jackyscript@users.noreply.github.com>
2023-06-17 19:59:44 +0200

    Refactor typewriter

--- assets/modules/typewriter.js
+++ assets/modules/typewriter.js
@@ -1,30 +1,48 @@
 export var typeWriterNotWriting = true;
 
+var gameTextElement = document.getElementById("gameText");
+
 function resetTextArea() {
-    document.getElementById("gameText").innerHTML = "";
+    gameTextElement.textContent = "";
 }
 
 export function typewriter(txt) {
     var i = 0;
-    var txt = txt;
     var speed = 10; /* The speed/duration of the effect in milliseconds */
 
     resetTextArea();
 
     function typewriterInternal() {
+
         if (i < txt.length) {
-            typeWriterNotWriting = true
-            var currentChar = txt.charAt(i);
-            if (currentChar.includes('.') || currentChar.includes('!'))
-                speed = 400;
-            else
-                speed = 10;
-            document.getElementById("gameText").innerHTML += currentChar;
-            i++;
-            setTimeout(typewriterInternal, speed);
+            writeText();
         } else {
             typeWriterNotWriting = false;
         }
+
+        function writeText() {
+
+            typeWriterNotWriting = true;
+            var currentChar = txt.charAt(i);
+            setupTextSpeed(currentChar);
+            gameTextElement.textContent += currentChar;
+            i++;
+            setTimeout(typewriterInternal, speed);
+
+        }
+
+        function setupTextSpeed(currentChar) {
+
+            if (currentChar.includes('.') || currentChar.includes('!'))
+                speed = 400;
+
+            else
+                speed = 10;
+
+        }
+
     }
+
     typewriterInternal(txt);
+
 }
